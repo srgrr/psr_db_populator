29bc6e5b896df5aab51553d06477c2371987bce1
Sergio R.Guasch <sergio.rodriguez@streamsets.com>
2023-03-21 12:16:08 -0700

    Support for creating org from JSONs

--- populator/load_schema_to_sch.py
+++ populator/load_schema_to_sch.py
@@ -1,8 +1,30 @@
 import logging
 import tqdm
+import uuid
+import json
 from populator.load_schema_cli import parse_args
 from populator.util import pretty_cli
-from streamsets import sdk
+from streamsets.sdk import ControlHub
+from streamsets.sdk.sch_models import Organization
+
+
+def _get_sdk_version():
+    from streamsets import sdk
+    return sdk.__version__
+
+
+def _get_sch(sch_url: str, sch_username: str, sch_password: str) -> ControlHub:
+    # TODO: ADAPT IT SO WE ALSO SUPPORT 4X
+    return ControlHub(sch_url, sch_username, sch_password)
+
+
+def _get_org_object_from_repr(sch, org_repr: dict) -> Organization:
+    org_name = org_repr.get("org_name")
+    return sch.get_organization_builder().build(id=org_name,
+                                                name=org_name,
+                                                admin_user_id=f'admin@{org_name}',
+                                                admin_user_display_name=f'{org_name} Admin',
+                                                admin_user_email_address='fake@example.com')
 
 
 def main(
@@ -12,9 +34,15 @@ def main(
     sch_password: str,
     sch_authoring_sdc: str
 ):
-    logging.debug(f"Using StreamSets SDK Version {sdk.__version__}")
+    logging.debug(f"Using StreamSets SDK Version {_get_sdk_version()}")
     logging.debug(f"Invoked PSR Load Schema to SCH tool with CLI args {pretty_cli(locals())}")
-    pass
+    admin_sch = _get_sch(sch_url, sch_username, sch_password)
+    for org_repr in json.load(open(json_file, "r")):
+        logging.debug(f"Processing org {org_repr.get('org_name')}")
+        org = _get_org_object_from_repr(admin_sch, org_repr)
+        logging.debug(f"Obtained SDK org from repr: {org}")
+        admin_sch.add_organization(org)
+        logging.debug(f"Added Org {org_repr.get('org_name')} to SCH")
 
 
 if __name__ == "__main__":
